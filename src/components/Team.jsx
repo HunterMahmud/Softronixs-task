e7406bedf71719fc7c8d45d46395b3e1d013bae1
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-19 19:12:03 +0600

    Team: hover effect added

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,13 +7,30 @@ const Team = () => {
                 <div className='flex justify-center items-center'>
                     <h1 className='text-center text-[30px] lg:text-[52px] text-white font-bold'>Meet the experts behind your success</h1>
                 </div>
-                {/* image */}
+
+                {/* image of team */}
                 <div className='flex items-center justify-between gap-6'>
-                    <div className='w-[306px] h-[380px] hidden lg:block'>
-                        <img className='w-[306px] h-[380px]' src="/team1.png" alt="" />
+                    <div className="w-[306px] h-[380px] hidden lg:block">
+                        <div className="relative w-[306px] h-[380px] group">
+                            {/* Image */}
+                            <img
+                                className="w-full h-full object-cover"
+                                src="/team1.png"
+                                alt="Team Member"
+                            />
+
+                            {/* Custom Gradient Overlay */}
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out before:absolute before:inset-0 before:bg-[linear-gradient(180deg,rgba(0,32,109,0)_50.47%,#00206D_100%)] before:z-10"></div>
+
+                            {/* Text Overlay */}
+                            <div className="absolute inset-0 flex flex-col ml-6 justify-end pb-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out z-20">
+                                <h5 className="text-[24px] text-[#F6F5F3] font-bold">Ronald Richards</h5>
+                                <p className="text-[#F6F5F3]">Finance Advisor</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='w-[306px] h-[380px] mx-auto lg:mx-0'>
-                        <div className="relative w-[306px] h-[380px]">
+                    <div className="w-[306px] h-[380px] mx-auto lg:mx-0">
+                        <div className="relative w-[306px] h-[380px] group">
                             {/* Image */}
                             <img
                                 className="w-full h-full object-cover"
@@ -22,24 +39,52 @@ const Team = () => {
                             />
 
                             {/* Custom Gradient Overlay */}
-                            <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[linear-gradient(180deg,rgba(0,32,109,0)_50.47%,#00206D_100%)] before:z-10"></div>
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out before:absolute before:inset-0 before:bg-[linear-gradient(180deg,rgba(0,32,109,0)_50.47%,#00206D_100%)] before:z-10"></div>
 
                             {/* Text Overlay */}
-                            <div className="absolute inset-0 flex flex-col ml-6 justify-end pb-6 text-white z-20">
+                            <div className="absolute inset-0 flex flex-col ml-6 justify-end pb-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out z-20">
                                 <h5 className="text-[24px] text-[#F6F5F3] font-bold">Ronald Richards</h5>
                                 <p className="text-[#F6F5F3]">Finance Advisor</p>
                             </div>
                         </div>
-
-
-                    </div>
-                    <div className='w-[306px] h-[380px]  hidden lg:block'>
-                        <img className='w-[306px] h-[380px]' src="/team3.png" alt="" />
-                    </div>
-                    <div className='w-[306px] h-[380px] hidden xl:block'>
-                        <img className='w-[306px] h-[380px]' src="/team4.jpeg" alt="" />
                     </div>
 
+                    <div className="w-[306px] h-[380px] hidden lg:block">
+                        <div className="relative w-[306px] h-[380px] group">
+                            {/* Image */}
+                            <img
+                                className="w-full h-full object-cover"
+                                src="/team3.png"
+                                alt="Team Member"
+                            />
+                            {/* Custom Gradient Overlay */}
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 before:absolute before:inset-0 before:bg-[linear-gradient(180deg,rgba(0,32,109,0)_50.47%,#00206D_100%)] before:z-10"></div>
+
+                            {/* Text Overlay */}
+                            <div className="absolute inset-0 flex flex-col ml-6 justify-end pb-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+                                <h5 className="text-[24px] text-[#F6F5F3] font-bold">Ronald Richards</h5>
+                                <p className="text-[#F6F5F3]">Finance Advisor</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[306px] h-[380px] hidden xl:block">
+                        <div className="relative w-[306px] h-[380px] group">
+                            {/* Image */}
+                            <img
+                                className="w-full h-full object-cover"
+                                src="/team4.jpeg"
+                                alt="Team Member"
+                            />
+                            {/* Custom Gradient Overlay */}
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 before:absolute before:inset-0 before:bg-[linear-gradient(180deg,rgba(0,32,109,0)_50.47%,#00206D_100%)] before:z-10"></div>
+
+                            {/* Text Overlay */}
+                            <div className="absolute inset-0 flex flex-col ml-6 justify-end pb-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+                                <h5 className="text-[24px] text-[#F6F5F3] font-bold">Ronald Richards</h5>
+                                <p className="text-[#F6F5F3]">Finance Advisor</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='flex justify-center items-center gap-3'>
                     <img className='w-2 h-2' src="/Ellipsewhite.png" alt="" />
