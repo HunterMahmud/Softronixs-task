977414d3a4a92717cbcf5c18fd13a4df393bbf88
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-21 17:08:38 +0600

    update: homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,23 +12,23 @@ import Testimonials from '../components/Testimonials';
 import WorkPolicy from '../components/WorkPolicy';
 
 const Home = () => {
-    return (
-        <div className='mx-auto font-Grotesk'>
-             <Navbar />
-            <div className='h-[1px] bg-[#1716164d] w-full'>
-            </div>
-             <Banner/> 
-           <Services/>
-             <AboutUs/>
-          <WorkPolicy/>
-              <Project/>
-            <Team/>
-           <Testimonials/>
-             <ConnectWithUs/>
-            <Blog/>
-           <Footer/>
-        </div>
-    );
+  return (
+    <div className='mx-auto font-Grotesk'>
+      <Navbar />
+      <div className='h-[1px] bg-[#1716164d] w-full'>
+      </div>
+      <Banner />
+      <Services />
+      <AboutUs />
+      <WorkPolicy />
+      <Project />
+      <Team />
+      <Testimonials />
+      <ConnectWithUs />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
