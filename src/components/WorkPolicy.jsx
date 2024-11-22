576c49e4d443c20f77615cba1de33a52dfb155d5
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-22 22:17:43 +0600

    update: unwanted code removed

--- src/components/WorkPolicy.jsx
+++ src/components/WorkPolicy.jsx
@@ -90,37 +90,3 @@ const WorkPolicy = () => {
 };
 
 export default WorkPolicy;
-
-
-
-
-
-/*
-
-<div className='p-10 bg-white space-y-16 h-[500px] md:h-[550px] lg:h-[500px] max-w-[400px] md:max-w-full mx-auto'>
-                        <div className='flex items-center justify-center w-20 h-20 bg-[#0147FF]'>
-                            <img className='w-12 h-12' src="/icon-planning.png" alt="" />
-                        </div>
-                        <div className='space-y-4'>
-                            <h4 className='text-[24px] font-bold'>Finance Planning</h4>
-                            <p className='text-[18px] text-[#656C7E]'>Morem ipsum dolor sittemet adipiscing Morem ipsum dolor sittemet adipiscing elitflorai psum dolor psum.</p>
-                            <div className='flex items-center gap-1 text-[#0147FF]'>
-                                <button className=' font-bold '>Read More</button>
-                                <FaArrowRight className='w-3 h-3 font-normal' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='p-10 bg-white space-y-16 hidden h-[500px] md:h-[550px] lg:h-[500px] md:block mx-auto'>
-                        <div className='flex items-center justify-center w-20 h-20 bg-[#F2F6FF]'>
-                            <img className='w-12 h-12' src="/icon-auditing.png" alt="" />
-                        </div>
-                        <div className='space-y-4'>
-                            <h4 className='text-[24px] font-bold'>Audit {'&'} Assurance</h4>
-                            <p className='text-[18px] text-[#656C7E]'>Morem ipsum dolor sittemet adipiscing Morem ipsum dolor sittemet adipiscing elitflorai psum dolor psum.</p>
-                            <div className='flex items-center gap-1 text-[#0147FF]'>
-                                <button className=' font-bold '>Read More</button>
-                                <FaArrowRight className='w-3 h-3 font-normal' />
-                            </div>
-                        </div>
-                    </div>
-*/
