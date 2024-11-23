d2561b598fbae5591fafbe59fe6a07412b639987
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-23 08:33:33 +0600

    add: navigation added on the testimonilas page

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,54 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 
 const Testimonials = () => {
+    const testimonials = [
+        {
+            id: 1,
+            name: "Marvin McKinney",
+            role: "Product Manager",
+            review:
+                "Working with several word themes and templates the last years only can say this is best in every level use it for my reviews that I have already are already company and the reviews that I have already are all excellent. Not only the design but the are company and the reviews code",
+            image: "./testimoni1.png",
+        },
+        {
+            id: 2,
+            name: "Jane Cooper",
+            role: "Software Engineer",
+            review:
+                "This platform has been a game-changer for my work! The templates are easy to use and perfectly designed. Highly recommended!",
+            image: "./testimoni2.png",
+        },
+        {
+            id: 3,
+            name: "Albert Flores",
+            role: "CEO, TechWorld",
+            review:
+                "I love how customizable and user-friendly the templates are. They’ve saved me hours of work. Simply amazing!",
+            image: "./testimoni3.png",
+        },
+    ];
+
+    // State to track the current testimonial index
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    // Handlers for navigation
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+        );
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    // Current testimonial data
+    const currentTestimonial = testimonials[currentIndex];
+
     return (
         <div className='px-2 md:px-5 lg:px-16 xl:px-20 2xl:px-32 mx-auto my-[80px] lg:my-[124px] flex flex-col lg:flex-row justify-between gap-5'>
             <div className=' flex flex-col justify-between gap-5'>
@@ -31,58 +79,54 @@ const Testimonials = () => {
                     <button className='bg-[#0147FF] text-white font-bold py-5 px-9'>Make Appointment</button>
                 </div>
             </div>
-            {/* arrow 1 */}
-           <div className='hidden lg:block'>
-           <div className='flex justify-center items-center h-full'>
-                <div className='w-[60px] h-[60px] bg-[#F2F6FF] flex justify-center items-center rounded-full'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.9998 11.0006H4.41379L9.70679 5.70757C9.8023 5.61532 9.87848 5.50498 9.93089 5.38297C9.9833 5.26097 10.0109 5.12975 10.012 4.99697C10.0132 4.86419 9.98789 4.73251 9.93761 4.60962C9.88733 4.48672 9.81307 4.37507 9.71918 4.28117C9.62529 4.18728 9.51364 4.11303 9.39074 4.06275C9.26784 4.01247 9.13616 3.98717 9.00339 3.98832C8.87061 3.98947 8.73939 4.01706 8.61738 4.06947C8.49538 4.12188 8.38503 4.19806 8.29279 4.29357L1.29279 11.2936C1.10532 11.4811 1 11.7354 1 12.0006C1 12.2657 1.10532 12.52 1.29279 12.7076L8.29279 19.7076C8.48139 19.8897 8.73399 19.9905 8.99619 19.9882C9.25838 19.986 9.5092 19.8808 9.6946 19.6954C9.88001 19.51 9.98518 19.2592 9.98746 18.997C9.98974 18.7348 9.88894 18.4822 9.70679 18.2936L4.41379 13.0006H21.9998C22.265 13.0006 22.5194 12.8952 22.7069 12.7077C22.8944 12.5201 22.9998 12.2658 22.9998 12.0006C22.9998 11.7354 22.8944 11.481 22.7069 11.2935C22.5194 11.1059 22.265 11.0006 21.9998 11.0006Z" fill="#00206D" />
-                    </svg>
-                </div>
-            </div>
-           </div>
-             <div className='p-7 lg:max-w-[526px] 2xl:max-w-[700px] bg-[#F2F6FF] flex flex-col justify-around gap-5 '> {/* w-[526px] */}
 
-                <div className='flex gap-2'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z" fill="#ECB014" />
-                    </svg>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z" fill="#ECB014" />
-                    </svg>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z" fill="#ECB014" />
-                    </svg>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z" fill="#ECB014" />
-                    </svg>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z" fill="#ECB014" />
-                    </svg>
-
-                </div>
-                <p className='text-[18px] text-[#656C7E]'>“ Working with several word themes and templates the last years only can say this is best in every level use it for my reviews that I have already are already company and the reviews that I have already are all excellent. Not only the design but the are company and the reviews code ”</p>
-                <div className='flex items-center justify-start gap-3'>
-                    <img className='h-[62] w-[62px] rounded-full' src="./testimoni1.png" alt="" />
-                    <div className=''>
-                        <h5 className='font-bold text-[#180030] text-[20px]'>Marvin McKinney</h5>
-                        <p className='text-[#525D71] '>Product Manager</p>
+            <div className='hidden lg:block'>
+                <div className='flex justify-center items-center h-full'>
+                    <div className='w-[60px] h-[60px] bg-[#F2F6FF] flex justify-center items-center rounded-full cursor-pointer'
+                        onClick={handlePrev}>
+                        <GoArrowLeft className='h-6 w-6 text-[#00206D]' />
                     </div>
                 </div>
             </div>
-            {/* arrow 2 */}
-            <div className='flex justify-center items-center gap-2 lg:gap-0 mt-5'>
-                <div className='w-[60px] h-[60px] bg-[#F2F6FF] lg:hidden flex justify-center items-center rounded-full'>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.9998 11.0006H4.41379L9.70679 5.70757C9.8023 5.61532 9.87848 5.50498 9.93089 5.38297C9.9833 5.26097 10.0109 5.12975 10.012 4.99697C10.0132 4.86419 9.98789 4.73251 9.93761 4.60962C9.88733 4.48672 9.81307 4.37507 9.71918 4.28117C9.62529 4.18728 9.51364 4.11303 9.39074 4.06275C9.26784 4.01247 9.13616 3.98717 9.00339 3.98832C8.87061 3.98947 8.73939 4.01706 8.61738 4.06947C8.49538 4.12188 8.38503 4.19806 8.29279 4.29357L1.29279 11.2936C1.10532 11.4811 1 11.7354 1 12.0006C1 12.2657 1.10532 12.52 1.29279 12.7076L8.29279 19.7076C8.48139 19.8897 8.73399 19.9905 8.99619 19.9882C9.25838 19.986 9.5092 19.8808 9.6946 19.6954C9.88001 19.51 9.98518 19.2592 9.98746 18.997C9.98974 18.7348 9.88894 18.4822 9.70679 18.2936L4.41379 13.0006H21.9998C22.265 13.0006 22.5194 12.8952 22.7069 12.7077C22.8944 12.5201 22.9998 12.2658 22.9998 12.0006C22.9998 11.7354 22.8944 11.481 22.7069 11.2935C22.5194 11.1059 22.265 11.0006 21.9998 11.0006Z" fill="#00206D" />
-                    </svg>
+            <div className='p-7 lg:max-w-[526px] min-h-[300px] bg-[#F2F6FF] flex flex-col justify-around gap-5'>
+
+                <div className='flex gap-2'>
+
+                    {[...Array(5)].map((_, index) => (
+                        <svg
+                            key={index}
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M11.9998 17L6.12197 20.5902L7.72007 13.8906L2.48926 9.40983L9.35479 8.85942L11.9998 2.5L14.6449 8.85942L21.5104 9.40983L16.2796 13.8906L17.8777 20.5902L11.9998 17Z"
+                                fill="#ECB014"
+                            />
+                        </svg>
+                    ))}
 
                 </div>
-                <div className='w-[60px] h-[60px] bg-[#F2F6FF]  flex justify-center items-center rounded-full'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2.00021 11.0006H19.5862L14.2932 5.70757C14.1977 5.61532 14.1215 5.50498 14.0691 5.38297C14.0167 5.26097 13.9891 5.12975 13.988 4.99697C13.9868 4.86419 14.0121 4.73251 14.0624 4.60962C14.1127 4.48672 14.1869 4.37507 14.2808 4.28117C14.3747 4.18728 14.4864 4.11303 14.6093 4.06275C14.7322 4.01247 14.8638 3.98717 14.9966 3.98832C15.1294 3.98947 15.2606 4.01706 15.3826 4.06947C15.5046 4.12188 15.615 4.19806 15.7072 4.29357L22.7072 11.2936C22.8947 11.4811 23 11.7354 23 12.0006C23 12.2657 22.8947 12.52 22.7072 12.7076L15.7072 19.7076C15.5186 19.8897 15.266 19.9905 15.0038 19.9882C14.7416 19.986 14.4908 19.8808 14.3054 19.6954C14.12 19.51 14.0148 19.2592 14.0125 18.997C14.0103 18.7348 14.1111 18.4822 14.2932 18.2936L19.5862 13.0006H2.00021C1.735 13.0006 1.48064 12.8952 1.29311 12.7077C1.10557 12.5201 1.00021 12.2658 1.00021 12.0006C1.00021 11.7354 1.10557 11.481 1.29311 11.2935C1.48064 11.1059 1.735 11.0006 2.00021 11.0006Z" fill="#00206D" />
-                    </svg>
+                <p className='text-[18px] text-[#656C7E]'>“ {currentTestimonial.review} ”</p>
+                <div className='flex items-center justify-start gap-3'>
+                    <img className='h-[62] w-[62px] rounded-full' src={`${currentTestimonial.image}`} alt="" />
+                    <div className=''>
+                        <h5 className='font-bold text-[#180030] text-[20px]'>{currentTestimonial.name}</h5>
+                        <p className='text-[#525D71] '>{currentTestimonial.role}</p>
+                    </div>
+                </div>
+            </div>
 
+            <div className='flex justify-center items-center gap-2 lg:gap-0 mt-5'>
+                <div className='w-[60px] h-[60px] bg-[#F2F6FF] lg:hidden flex justify-center items-center rounded-full cursor-pointer'
+                    onClick={handlePrev}>
+                    <GoArrowLeft className='h-6 w-6 text-[#00206D]' />
+                </div>
+                <div className='w-[60px] h-[60px] bg-[#F2F6FF]  flex justify-center items-center rounded-full cursor-pointer'
+                    onClick={handleNext}>
+                    <GoArrowRight className='h-6 w-6 text-[#00206D]' />
                 </div>
             </div>
 
